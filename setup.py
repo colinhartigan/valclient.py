d18c71e76e42e4b606dca7a3da25fb41bcd8490f
Colin <colinjoe9@gmail.com>
2021-08-30 21:00:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="valclient", # Replace with your own username
-    version="2.19",
+    version="2.20",
     author="colinh",
     description="Wrapper for VALORANT's client API",
     long_description=long_desc,
